237beafe4d5e030f894eccc74f9a0aac1e5a0a41
mdr-leon10 <mdr.leon10@uniandes.edu.co>
2017-10-16 18:36:49 -0500

    Add files via upload

---
+++ decide/decide-back/src/main/java/co/edu/uniandes/csw/decide/ejb/DefaultLogic.java
@@ -0,0 +1,80 @@
+/*
+MIT License
+
+Copyright (c) 2017 Universidad de los Andes - ISIS2603
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package co.edu.uniandes.csw.decide.ejb;
+
+
+
+import co.edu.uniandes.csw.decide.entities.DefaultEntity;
+import co.edu.uniandes.csw.decide.exceptions.BusinessLogicException;
+
+import co.edu.uniandes.csw.decide.persistence.DefaultPersistence;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author ISIS2603
+ */
+@Stateless
+public class DefaultLogic {
+
+    private static final Logger LOGGER = Logger.getLogger(DefaultLogic.class.getName());
+
+    @Inject
+    private DefaultPersistence persistence; // Variable para acceder a la persistencia de la aplicación. Es una inyección de dependencias.
+
+    /**
+     *
+     * @param entity
+     * @return
+     * @throws BusinessLogicException
+     */
+    public DefaultEntity createDefault(DefaultEntity entity) throws BusinessLogicException {
+        LOGGER.info("Inicia proceso de creación de Default");
+        // Invoca la persistencia para crear la Default
+        persistence.create(entity);
+        LOGGER.info("Termina proceso de creación de Default");
+        return entity;
+    }
+
+    /**
+     * 
+     * Obtener todas las Defaultes existentes en la base de datos.
+     *
+     * @return una lista de Defaultes.
+     */
+    public List<DefaultEntity> getDefaults() {
+        LOGGER.info("Inicia proceso de consultar todas las Defaultes");
+        // Note que, por medio de la inyección de dependencias se llama al método "findAll()" que se encuentra en la persistencia.
+        List<DefaultEntity> Default = persistence.findAll();
+        LOGGER.info("Termina proceso de consultar todas las Defaultes");
+        return Default;
+    }
+
+
+}
---
+++ decide/decide-back/src/main/java/co/edu/uniandes/csw/decide/persistence/DefaultPersistence.java
@@ -0,0 +1,126 @@
+/*
+MIT License
+
+Copyright (c) 2017 Universidad de los Andes - ISIS2603
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package co.edu.uniandes.csw.decide.persistence;
+
+
+
+import co.edu.uniandes.csw.decide.entities.DefaultEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author ISIS2603
+ */
+@Stateless
+public class DefaultPersistence {
+
+    private static final Logger LOGGER = Logger.getLogger(DefaultPersistence.class.getName());
+
+    @PersistenceContext(unitName = "decidePU")
+    protected EntityManager em;
+
+    /**
+     *
+     * @param entity objeto Default que se creará en la base de datos
+     * @return devuelve la entidad creada con un id dado por la base de datos.
+     */
+    public DefaultEntity create(DefaultEntity entity) {
+        LOGGER.info("Creando un default nuevo");
+        /* Note que hacemos uso de un método propio de EntityManager para persistir la Default en la base de datos.
+        Es similar a "INSERT INTO table_codigo (column1, column2, column3, ...) VALUES (value1, value2, value3, ...);" en SQL.
+         */
+        em.persist(entity);
+        LOGGER.info("Creando un default nuevo");
+        return entity;
+    }
+
+    /**
+     * Actualiza un default.
+     *
+     * @param entity: el default que viene con los nuevos cambios. Por ejemplo
+     * el codigo pudo cambiar. En ese caso, se haria uso del método update.
+     * @return un default con los cambios aplicados.
+     */
+    public DefaultEntity update(DefaultEntity entity) {
+        LOGGER.log(Level.INFO, "Actualizando Default con id={0}", entity.getId());
+        /* Note que hacemos uso de un método propio del EntityManager llamado merge() que recibe como argumento
+        la Default con los cambios, esto es similar a 
+        "UPDATE table_codigo SET column1 = value1, column2 = value2, ... WHERE condition;" en SQL.
+         */
+        return em.merge(entity);
+    }
+
+    /**
+     *
+     * Borra un biblioteca de la base de datos recibiendo como argumento el id
+     * de la Default
+     *
+     * @param id: id correspondiente a la Default a borrar.
+     */
+    public void delete(Long id) {
+        LOGGER.log(Level.INFO, "Borrando Default con id={0}", id);
+        // Se hace uso de mismo método que esta explicado en public DefaultEntity find(Long id) para obtener la Default a borrar.
+        DefaultEntity entity = em.find(DefaultEntity.class, id);
+        /* Note que una vez obtenido el objeto desde la base de datos llamado "entity", volvemos hacer uso de un método propio del
+         EntityManager para eliminar de la base de datos el objeto que encontramos y queremos borrar.
+         Es similar a "delete from DefaultEntity where id=id;" - "DELETE FROM table_codigo WHERE condition;" en SQL.*/
+        em.remove(entity);
+    }
+
+    /**
+     * Busca si hay algun default con el id que se envía de argumento
+     *
+     * @param id: id correspondiente a la Default buscada.
+     * @return un default.
+     */
+    public DefaultEntity find(Long id) {
+        LOGGER.log(Level.INFO, "Consultando Default con id={0}", id);
+        /* Note que se hace uso del metodo "find" propio del EntityManager, el cual recibe como argumento 
+        el tipo de la clase y el objeto que nos hara el filtro en la base de datos en este caso el "id"
+        Suponga que es algo similar a "select * from DefaultEntity where id=id;" - "SELECT * FROM table_codigo WHERE condition;" en SQL.
+         */
+        return em.find(DefaultEntity.class, id);
+    }
+
+    /**
+     * Devuelve todas las Default de la base de datos.
+     *
+     * @return una lista con todas las Default que encuentre en la base de
+     * datos, "select u from DefaultEntity u" es como un "select * from
+     * DefaultEntity;" - "SELECT * FROM table_codigo" en SQL.
+     */
+    public List<DefaultEntity> findAll() {
+        LOGGER.info("Consultando todas las Default");
+        // Se crea un query para buscar todas las Default en la base de datos.
+        TypedQuery query = em.createQuery("select u from DefaultEntity u", DefaultEntity.class);
+        // Note que en el query se hace uso del método getResultList() que obtiene una lista de Default.
+        return query.getResultList();
+    }
+}
